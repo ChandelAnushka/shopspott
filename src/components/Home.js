9744d0b1735c6e1b705c12d3631ce380fd12650b
ChandelAnushka <chandelanushka99@gmail.com>
2020-08-13 13:42:01 +0530

    Home in progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,6 +19,23 @@ import p5 from "./img/product/p5.jpg";
 import p6 from "./img/product/p6.jpg";
 import p7 from "./img/product/p7.jpg";
 import p8 from "./img/product/p8.jpg";
+import e_p1 from "./img/product/e-p1.png";
+import b1 from "./img/brand/1.png";
+import b2 from "./img/brand/2.png";
+import b3 from "./img/brand/3.png";
+import b4 from "./img/brand/4.png";
+import b5 from "./img/brand/5.png";
+import r1 from "./img/deals/r1.jpg";
+import r2 from "./img/deals/r2.jpg";
+import r3 from "./img/deals/r3.jpg";
+import r4 from "./img/deals/r4.jpg";
+import r6 from "./img/deals/r6.jpg";
+import r7 from "./img/deals/r7.jpg";
+import r9 from "./img/deals/r9.jpg";
+import r10 from "./img/deals/r10.jpg";
+import r11 from "./img/deals/r11.jpg";
+
+
 
 
 function Home() {
@@ -534,7 +551,7 @@ function Home() {
 					{/* <!-- single product --> */}
 					<div class="col-lg-3 col-md-6">
 						<div class="single-product">
-							<img class="img-fluid" src="img/product/p6.jpg" alt=""></img>
+							<img class="img-fluid" src={p6} alt=""></img>
 							<div class="product-details">
 								<h6>addidas New Hammer sole
 									for Sports person</h6>
@@ -567,7 +584,7 @@ function Home() {
 					{/* <!-- single product --> */}
 					<div class="col-lg-3 col-md-6">
 						<div class="single-product">
-							<img class="img-fluid" src="img/product/p8.jpg" alt=""></img>
+							<img class="img-fluid" src={p8} alt=""></img>
 							<div class="product-details">
 								<h6>addidas New Hammer sole
 									for Sports person</h6>
@@ -600,7 +617,7 @@ function Home() {
 					{/* <!-- single product --> */}
 					<div class="col-lg-3 col-md-6">
 						<div class="single-product">
-							<img class="img-fluid" src="img/product/p3.jpg" alt=""></img>
+							<img class="img-fluid" src={p3} alt=""></img>
 							<div class="product-details">
 								<h6>addidas New Hammer sole
 									for Sports person</h6>
@@ -633,7 +650,7 @@ function Home() {
 					{/* <!-- single product --> */}
 					<div class="col-lg-3 col-md-6">
 						<div class="single-product">
-							<img class="img-fluid" src="img/product/p5.jpg" alt=""></img>
+							<img class="img-fluid" src={p5} alt=""></img>
 							<div class="product-details">
 								<h6>addidas New Hammer sole
 									for Sports person</h6>
@@ -666,7 +683,7 @@ function Home() {
 					{/* <!-- single product --> */}
 					<div class="col-lg-3 col-md-6">
 						<div class="single-product">
-							<img class="img-fluid" src="img/product/p1.jpg" alt=""></img>
+							<img class="img-fluid" src={p1} alt=""></img>
 							<div class="product-details">
 								<h6>addidas New Hammer sole
 									for Sports person</h6>
@@ -699,7 +716,7 @@ function Home() {
 					{/* <!-- single product --> */}
 					<div class="col-lg-3 col-md-6">
 						<div class="single-product">
-							<img class="img-fluid" src="img/product/p4.jpg" alt=""></img>
+							<img class="img-fluid" src={p4} alt=""></img>
 							<div class="product-details">
 								<h6>addidas New Hammer sole
 									for Sports person</h6>
@@ -732,7 +749,7 @@ function Home() {
 					{/* <!-- single product --> */}
 					<div class="col-lg-3 col-md-6">
 						<div class="single-product">
-							<img class="img-fluid" src="img/product/p1.jpg" alt=""></img>
+							<img class="img-fluid" src={p1} alt=""></img>
 							<div class="product-details">
 								<h6>addidas New Hammer sole
 									for Sports person</h6>
@@ -765,7 +782,7 @@ function Home() {
 					{/* <!-- single product --> */}
 					<div class="col-lg-3 col-md-6">
 						<div class="single-product">
-							<img class="img-fluid" src="img/product/p8.jpg" alt=""></img>
+							<img class="img-fluid" src={p8} alt=""></img>
 							<div class="product-details">
 								<h6>addidas New Hammer sole
 									for Sports person</h6>
@@ -800,6 +817,323 @@ function Home() {
 		</div>
 	</div>
 	{/* <!-- end product Area --> */}
+
+
+
+
+
+	{/* <!-- Start exclusive deal Area --> */}
+	<div class="exclusive-deal-area">
+		<div class="container-fluid">
+			<div class="row justify-content-center align-items-center">
+				<div class="col-lg-6 no-padding exclusive-left">
+					<div class="row clock_sec clockdiv" id="clockdiv">
+						<div class="col-lg-12">
+							<h1>Exclusive Hot Deal Ends Soon!</h1>
+							<p>Who are in extremely love with eco friendly system.</p>
+						</div>
+						<div class="col-lg-12">
+							<div class="row clock-wrap">
+								<div class="col clockinner1 clockinner">
+									<h1 class="days">150</h1>
+									<span class="smalltext">Days</span>
+								</div>
+								<div class="col clockinner clockinner1">
+									<h1 class="hours">23</h1>
+									<span class="smalltext">Hours</span>
+								</div>
+								<div class="col clockinner clockinner1">
+									<h1 class="minutes">47</h1>
+									<span class="smalltext">Mins</span>
+								</div>
+								<div class="col clockinner clockinner1">
+									<h1 class="seconds">59</h1>
+									<span class="smalltext">Secs</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<a href="" class="primary-btn">Shop Now</a>
+				</div>
+				<div class="col-lg-6 no-padding exclusive-right">
+					<div class="active-exclusive-product-slider">
+						{/* <!-- single exclusive carousel --> */}
+						<div class="single-exclusive-slider">
+							<img class="img-fluid" src={e_p1} alt=""></img>
+							<div class="product-details">
+								<div class="price">
+									<h6>$150.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+								<h4>addidas New Hammer sole
+									for Sports person</h4>
+								<div class="add-bag d-flex align-items-center justify-content-center">
+									<a class="add-btn" href=""><span class="ti-bag"></span></a>
+									<span class="add-text text-uppercase">Add to Bag</span>
+								</div>
+							</div>
+						</div>
+						{/* <!-- single exclusive carousel --> */}
+						<div class="single-exclusive-slider">
+							<img class="img-fluid" src={e_p1} alt=""></img>
+							<div class="product-details">
+								<div class="price">
+									<h6>$150.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+								<h4>addidas New Hammer sole
+									for Sports person</h4>
+								<div class="add-bag d-flex align-items-center justify-content-center">
+									<a class="add-btn" href=""><span class="ti-bag"></span></a>
+									<span class="add-text text-uppercase">Add to Bag</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	{/* <!-- End exclusive deal Area --> */}
+
+
+	{/* <!-- Start brand Area --> */}
+	<div class="brand-area section_gap">
+		<div class="container">
+			<div class="row">
+				<a class="col single-img" href="#">
+					<img class="img-fluid d-block mx-auto" src={b1} alt=""></img>
+				</a>
+				<a class="col single-img" href="#">
+					<img class="img-fluid d-block mx-auto" src={b2} alt=""></img>
+				</a>
+				<a class="col single-img" href="#">
+					<img class="img-fluid d-block mx-auto" src={b3} alt=""></img>
+				</a>
+				<a class="col single-img" href="#">
+					<img class="img-fluid d-block mx-auto" src={b4} alt=""></img>
+				</a>
+				<a class="col single-img" href="#">
+					<img class="img-fluid d-block mx-auto" src={b5} alt=""></img>
+				</a>
+			</div>
+		</div>
+	</div>
+	{/* <!-- End brand Area --></a> */}
+
+
+	{/* <!-- Start Deals of the Week Area --> */}
+	<div class="related-product-area section_gap_bottom">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-6 text-center">
+					<div class="section-title">
+						<h1>Deals of the Week</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+							magna aliqua.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-9">
+					<div class="row">
+						<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+							<div class="single-related-product d-flex">
+								<a href="#"><img src={r1} alt=""></img></a>
+								<div class="desc">
+									<a href="#" class="title">Black lace Heels</a>
+									<div class="price">
+										<h6>$189.00</h6>
+										<h6 class="l-through">$210.00</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+							<div class="single-related-product d-flex">
+								<a href="#"><img src={r2} alt=""></img></a>
+								<div class="desc">
+									<a href="#" class="title">Black lace Heels</a>
+									<div class="price">
+										<h6>$189.00</h6>
+										<h6 class="l-through">$210.00</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+							<div class="single-related-product d-flex">
+								<a href="#"><img src={r3} alt=""></img></a>
+								<div class="desc">
+									<a href="#" class="title">Black lace Heels</a>
+									<div class="price">
+										<h6>$189.00</h6>
+										<h6 class="l-through">$210.00</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+							<div class="single-related-product d-flex">
+								<a href="#"><img src={r4} alt=""></img></a>
+								<div class="desc">
+									<a href="#" class="title">Black lace Heels</a>
+									<div class="price">
+										<h6>$189.00</h6>
+										<h6 class="l-through">$210.00</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+							<div class="single-related-product d-flex">
+								<a href="#"><img src={r6} alt=""></img></a>
+								<div class="desc">
+									<a href="#" class="title">Black lace Heels</a>
+									<div class="price">
+										<h6>$189.00</h6>
+										<h6 class="l-through">$210.00</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+							<div class="single-related-product d-flex">
+								<a href="#"><img src={r7} alt=""></img></a>
+								<div class="desc">
+									<a href="#" class="title">Black lace Heels</a>
+									<div class="price">
+										<h6>$189.00</h6>
+										<h6 class="l-through">$210.00</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-related-product d-flex">
+								<a href="#"><img src={r9} alt=""></img></a>
+								<div class="desc">
+									<a href="#" class="title">Black lace Heels</a>
+									<div class="price">
+										<h6>$189.00</h6>
+										<h6 class="l-through">$210.00</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-related-product d-flex">
+								<a href="#"><img src={r10} alt=""></img></a>
+								<div class="desc">
+									<a href="#" class="title">Black lace Heels</a>
+									<div class="price">
+										<h6>$189.00</h6>
+										<h6 class="l-through">$210.00</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-related-product d-flex">
+								<a href="#"><img src={r11} alt=""></img></a>
+								<div class="desc">
+									<a href="#" class="title">Black lace Heels</a>
+									<div class="price">
+										<h6>$189.00</h6>
+										<h6 class="l-through">$210.00</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="ctg-right">
+						<a href="#" target="_blank">
+							<img class="img-fluid d-block mx-auto" src={c5} alt=""></img>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	{/* <!-- End related-product Area --> */}
+
+
+
+
+	{/* <!-- start footer Area --> */}
+	<footer class="footer-area section_gap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3  col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6>About Us</h6>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore
+							magna aliqua.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-4  col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6>Newsletter</h6>
+						<p>Stay update with our latest</p>
+						<div class="" id="mc_embed_signup">
+
+							<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+							 method="get" class="form-inline">
+
+								<div class="d-flex flex-row">
+
+									<input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '"
+									 required="" type="email"></input>
+
+
+									<button class="click-btn btn btn-default"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>
+									<div >
+										<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text"></input>
+									</div>
+
+				
+								</div>
+								<div class="info"></div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3  col-md-6 col-sm-6">
+					<div class="single-footer-widget mail-chimp">
+						<h6 class="mb-20">Instragram Feed</h6>
+						<ul class="instafeed d-flex flex-wrap">
+							<li><img src="img/i1.jpg" alt=""></img></li>
+							<li><img src="img/i2.jpg" alt=""></img></li>
+							<li><img src="img/i3.jpg" alt=""></img></li>
+							<li><img src="img/i4.jpg" alt=""></img></li>
+							<li><img src="img/i5.jpg" alt=""></img></li>
+							<li><img src="img/i6.jpg" alt=""></img></li>
+							<li><img src="img/i7.jpg" alt=""></img></li>
+							<li><img src="img/i8.jpg" alt=""></img></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6>Follow Us</h6>
+						<p>Let us be social</p>
+						<div class="footer-social d-flex align-items-center">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</footer>
+	{/* <!-- End footer Area --> */}
 
 </div>
   );
